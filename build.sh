1cebd095dc49141463e276f79090f32ab33d9e5c
峨眉山市雅铭网络工作室 <417733808@qq.com>
2021-12-10 19:44:59 +0800

    添加依赖工具

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@
 current_time=$(date "+%Y_%m_%d")
 
 
-iasl ./IMSx79/dsdt/DSDT.dsl
+./osx/bin/iasl ./IMSx79/dsdt/DSDT.dsl
 
 ./osx/bin/OZMTool --dsdtinject -i ./IMSx79/E7735IMS.280 -d ./IMSx79/dsdt/DSDT.aml -o ./build/E7735280_x79gd45_$current_time.bin
