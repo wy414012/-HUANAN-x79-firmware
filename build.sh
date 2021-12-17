078b094d94d180b7363bf20a44188868c673aca4
峨眉山市雅铭网络工作室 <417733808@qq.com>
2021-12-17 17:36:26 +0800

    修正一个路径错误

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@
 
 current_time=$(date "+%Y_%m_%d")
 
-./osx/bin/UEFIPatch ./X79MA-GD45/E7735IMS.280 -o ./build/E7735IMS.280$current_time.bin
+./osx/bin/UEFIPatch ./X79MA-GD45/E7735IMS.280 -o ./build/tmp/E7735IMS.280$current_time.bin
 
 ./osx/bin/iasl ./X79MA-GD45/dsdt/DSDT.dsl
 
-./osx/bin/OZMTool --dsdtinject -i ./build/E7735IMS.280$current_time.bin -d ./X79MA-GD45/dsdt/DSDT.aml -o ./build/E7735280_X79MA-GD45_$current_time.bin
+./osx/bin/OZMTool --dsdtinject -i ./build/tmp/E7735IMS.280$current_time.bin -d ./X79MA-GD45/dsdt/DSDT.aml -o ./build/E7735280_X79MA-GD45_$current_time.bin
 
 echo "构建完成开始生成sha256sum码"
 
