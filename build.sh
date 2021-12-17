b5da065f39473c3c96dbf25f52a1db417af92dad
峨眉山市雅铭网络工作室 <417733808@qq.com>
2021-12-17 17:42:21 +0800

    更新临时固件目录

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 #encoding=utf-8
 
 current_time=$(date "+%Y_%m_%d")
-
-./osx/bin/UEFIPatch ./X79MA-GD45/E7735IMS.280 -o ./build/tmp/E7735IMS.280$current_time.bin
+mkdir -p Patch
+./osx/bin/UEFIPatch ./X79MA-GD45/E7735IMS.280 -o ./Patch/E7735IMS.280$current_time.bin
 
 ./osx/bin/iasl ./X79MA-GD45/dsdt/DSDT.dsl
 
-./osx/bin/OZMTool --dsdtinject -i ./build/tmp/E7735IMS.280$current_time.bin -d ./X79MA-GD45/dsdt/DSDT.aml -o ./build/E7735280_X79MA-GD45_$current_time.bin
+./osx/bin/OZMTool --dsdtinject -i ./Patch/E7735IMS.280$current_time.bin -d ./X79MA-GD45/dsdt/DSDT.aml -o ./build/E7735280_X79MA-GD45_$current_time.bin
 
 echo "构建完成开始生成sha256sum码"
 
